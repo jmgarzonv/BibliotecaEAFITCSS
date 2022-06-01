77bfabb76c65f65eb991dc6fc8a27c79d4fad493
jmgarzonv <jmgarzonv@eafit.edu.co>
2022-05-31 21:24:22 -0500

    CSS in Progress

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import Menu from "../components/Menu";
+import InicioSesion from "./InicioSesion";
+
+export default function Inicio(){
+    return(
+        <div>  
+            <Menu/>
+            <h2>inicio Principal Manolo Was Here</h2>
+        </div>
+    )
+}
---
+++ src/pages/Libros.jsx
@@ -0,0 +1,91 @@
+import { Table } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import Menu from "../components/Menu"
+import './css/libros.css'
+
+export default function Libros(props){
+     
+    const [books, setBooks] = useState([])
+
+    var num = ''
+
+    const loadBook = async () => {
+        const response = await fetch(`http://localhost:4000/libros/${num}`)
+        const data = await response.json()
+        setBooks(data)
+    }
+
+    useEffect(() =>{
+        loadBook()
+    }, [])
+    
+    const booksMapAll =  books.map(book => {
+        return(
+            <tr>
+                <th scope="row">
+                    {book.idlibro}
+                </th>
+                <td>
+                    {book.titulolibro}
+                </td>
+                <td>
+                    {book.nombreautor}
+                </td>
+                <td>
+                    {book.editoriallibro}
+                </td>
+                <td>
+                    {book.arealibro}
+                </td>
+            </tr>
+        );
+    })   
+
+    return(
+        <div>
+            <Menu></Menu>
+            <Table
+                bordered
+                hover
+                responsive
+                >
+                <thead>
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Titulo
+                        </th>
+                        <th>
+                            Autor
+                        </th>
+                        <th>
+                            Editorial
+                        </th>
+                        <th>
+                            Area
+                        </th>
+                    </tr>
+                    <tr class="test">
+                        <td>Libro1</td>
+                        <td>Libro2</td>
+                        <td>Libro3</td>
+                        <td>Libro4</td>
+                        <td>Libro5</td>
+                    </tr>
+                    <tr>
+                        <td>Libro1</td>
+                        <td>Libro2</td>
+                        <td>Libro3</td>
+                        <td>Libro4</td>
+                        <td>Libro5</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {booksMapAll}
+                </tbody>
+            </Table>
+        </div>
+    )
+}
