9ceef03c40673361804dc495fb0868fc9b37195d
Alejo1214 <alejocar1214@gmail.com>
2022-06-02 14:44:00 -0500

    InicioCss

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,11 +1,41 @@
 import Menu from "../components/Menu";
+import "../pages/css/inicio.css";
 import InicioSesion from "./InicioSesion";
 
 export default function Inicio(){
     return(
         <div>  
             <Menu/>
-            <h2>inicio Principal</h2>
+
+            <div id="kiko-slider">
+  
+  <ul class="slider">
+    <li id="slide1">
+      <img src="https://i.ibb.co/3TSsKxf/bibliotc.jpg"/>
+    </li>
+    <li id="slide2">
+      <img src="https://i.ibb.co/pb0Hgj7/libros.jpg"/>
+    </li>
+    <li id="slide3">
+      <img src="https://i.ibb.co/wNcTFwf/animat.jpg"/>
+    </li>
+  </ul>
+  
+  <ul class="menu">
+    <li>
+      <a href="#slide1">〇</a>
+    </li>
+    <li>
+      <a href="#slide2">〇</a>
+    </li>
+     <li>
+      <a href="#slide3">〇</a>
+    </li>
+  </ul>
+
+  
+</div>
         </div>
+        
     )
 }
